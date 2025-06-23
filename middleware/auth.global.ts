f6d6186927ec078d128e4621e098dbf355ed640d
Sapolayot Nantawong <sapolayotnan@gosoft.co.th>
2025-06-23 21:28:38 +0700

    Improve middleware authen

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,6 +1,10 @@
 export default defineNuxtRouteMiddleware((to, from) => {
+    const guestOnlyPaths = ["/login", "/register"];
+
     const token = useCookie("access_token");
-    if (!token.value && to.path !== "/login") {
+    if (!token.value && !guestOnlyPaths.includes(to.path)) {
         return navigateTo("/login");
+    } else if (token.value && guestOnlyPaths.includes(to.path)) {
+        return navigateTo("/book-management");
     }
 });
