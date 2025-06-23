983a8516cb8a3dc0d44ee0305db027f9b9df4d19
Sapolayot Nantawong <sapolayotnan@gosoft.co.th>
2025-06-23 21:16:26 +0700

    Fix response 401

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -13,6 +13,26 @@ export default defineNuxtPlugin(() => {
         baseURL: config.public.apiBase,
     });
 
+    api.interceptors.request.use((req) => {
+        const access_token = useCookie("access_token");
+        if (access_token.value) {
+            req.headers.Authorization = `Bearer ${access_token.value}`;
+        }
+
+        return req;
+    });
+
+    api.interceptors.response.use(
+        (response) => response,
+        (error) => {
+            if (error.response && error.response.status === 401) {
+                const access_token = useCookie("access_token");
+                access_token.value = null;
+            }
+            return Promise.reject(error);
+        }
+    );
+
     return {
         provide: {
             api,
